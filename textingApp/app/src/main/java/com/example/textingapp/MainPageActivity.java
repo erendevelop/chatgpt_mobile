b9f359e524d7f449e140be5be3b4df28323ab4d4
eren <turhanerenmuhammed527@gmail.com>
2023-03-17 23:30:05 +0300

    ChatGPT Mobile codes.

---
+++ textingApp/app/src/main/java/com/example/textingapp/MainPageActivity.java
@@ -0,0 +1,27 @@
+package com.example.textingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainPageActivity extends AppCompatActivity {
+    Button useGPTButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        useGPTButton = findViewById(R.id.useGPT);
+        onClick();
+    }
+    private void onClick(){
+        useGPTButton.setOnClickListener(e -> {
+            Toast.makeText(this, "Routed to the chat page.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, BotActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ textingApp/app/src/main/java/com/example/textingapp/MessageAdapter.java
@@ -0,0 +1,66 @@
+package com.example.textingapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>{
+    List<Message> messageList;
+    // getting the messages from list into messageList
+    public MessageAdapter(List<Message> messageList){
+        this.messageList = messageList;
+    }
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_layout, null);
+        MessageViewHolder viewHolder = new MessageViewHolder(chatView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messageList.get(position);
+        if(message.getSentBy().equals(Message.SENT_BY_ME)){
+            // setting the left message bubble invisible
+
+            holder.leftChatView.setVisibility(View.GONE);
+            // setting the right message bubble visible
+
+            holder.rightChatView.setVisibility(View.VISIBLE);
+
+            // adding message to textview
+            holder.rightChatTextView.setText(message.getMessage());
+        } else{
+            holder.rightChatView.setVisibility(View.GONE);
+            holder.leftChatView.setVisibility(View.VISIBLE);
+            holder.leftChatTextView.setText(message.getMessage());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftChatView, rightChatView;
+        TextView leftChatTextView, rightChatTextView;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            leftChatView = itemView.findViewById(R.id.left_chat_view);
+            rightChatView = itemView.findViewById(R.id.right_chat_view);
+
+            leftChatTextView = itemView.findViewById(R.id.left_text_view);
+            rightChatTextView = itemView.findViewById(R.id.right_text_view);
+        }
+    }
+}
